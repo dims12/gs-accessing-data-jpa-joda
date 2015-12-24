9e0869060bb84117ba88f8fe417dcbe139cc6096
dims12@mail.ru <dims12@mail.ru>
2015-12-24 18:42:25 +0300

    code sample

---
+++ src/main/java/hello/Customer.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.joda.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    private Instant created = new Instant();
+
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s', created='%s']",
+                id, firstName, lastName, created.toString());
+    }
+
+}
